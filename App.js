d08b1eb225a3f866baf0ae982e9c0a41cd219b93
Armando Gúzman <armando@ideamosweb.com>
2019-01-09 19:27:05 -0500

    clase 4

--- App.js
+++ App.js
@@ -1,53 +1,76 @@
 import React from 'react';
-import { StyleSheet, Text, View, Platform, TextInput, KeyboardAvoidingView, ImageBackground } from 'react-native';
+import { StyleSheet, Text, Platform,  KeyboardAvoidingView, ActivityIndicator,ImageBackground } from 'react-native';
 import SearchInput from  './src/components/SearchInput'
-import imageBackground from './assets/bg/clear.png'
-
 import { fetchLocationId, fetchWeatherById } from './src/api'
+
+import getWeatherBackground from './src/utils/getWeatherBackground'
 
 export default class App extends React.Component {
   state = { 
     text: '',
-    location: '',
+    city: '',
     weather: '',
-    temperature: ''
+    temperature: '',
+    isLoading: false,
+    error: false
+  }
+
+  componentWillMount () {
+    this._searchWeather("San Francisco")
   }
   
-  _handleChangeText = (text) => { this.setState({ text })
+  _handleChangeText = (text) => { this.setState({ text }) }
+
+  _handleSubmit = () => {
+    const { text } = this.state
+    if ( !text )return
+    this._searchWeather( text )
+    this.setState({ text: '' })
   }
 
-  _handleSubmit =  async () => {
-    const { text } = this.state
-
-    if ( !text ){
-      return
-    }else {
-      this.setState({ location: text })
-      this.setState({ text: '' })
-
-      const locationData =  await fetchLocationId( text )
+  _searchWeather = async ( location ) => {
+    try{
+      this.setState({ isLoading: !this.state.isLoading })
+      const locationData =  await fetchLocationId( location )
       const woeid = locationData[0].woeid
-      const weatherData = await fetchWeatherById( woeid )
 
-      const { weather, temperature } = weatherData
-      this.setState({ weather, temperature })
+      const weatherData = await fetchWeatherById( woeid )
+      const { weather, temperature, city } = weatherData
+      this.setState({ weather, temperature, city, isLoading: false, error: false })
+    }catch(error){
+      console.log(error)
+      this.setState({
+        error: true,
+        isLoading: false
+      })
     }
   }
 
   render() {
-    const { location, weather, temperature  } = this.state
+    const {  city, weather, temperature, isLoading, error  } = this.state
     return (
       <KeyboardAvoidingView
         style={styles.container}
         behavior="padding"
       >
         <ImageBackground
-          source={imageBackground}
+          source={getWeatherBackground(weather)}
           style={styles.imageBackground}
         >
-          <Text style={[styles.lasgeText, styles.textStyle]}>{ location }</Text>
-          <Text style={[styles.smallText, styles.textStyle]}>{ weather }</Text>
-          <Text style={[styles.lasgeText, styles.textStyle]}>{ Math.round(temperature) }º</Text>
+          {
+            isLoading 
+            ? <ActivityIndicator size="large"/>
+            : (
+                error
+                ? <Text style={[styles.smallText, styles.textStyle]}>Nojoda lo Dañaste</Text>
+                : <React.Fragment>
+                    <Text style={[styles.lasgeText, styles.textStyle]}>{ city }</Text>
+                    <Text style={[styles.smallText, styles.textStyle]}>{ weather }</Text>
+                    <Text style={[styles.lasgeText, styles.textStyle]}>{ Math.round(temperature) }º</Text>
+                  </React.Fragment>
+            )
+
+          }
           <SearchInput 
             placeholder="Search a Cool City"
             handleChangeText={ this._handleChangeText }
